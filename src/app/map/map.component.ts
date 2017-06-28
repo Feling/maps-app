9a0d1ffc1a40dbe9f8ca1e18d5fbb1ec69823f88
igork <feling87@gmail.com>
2017-06-28 17:11:15 +0300

    google maps @types

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,25 @@
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import { } from '@types/googlemaps';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  @Input('lat') public lat: number;
+  @Input('lan') public lan: number;
+  @Output() onClick = new EventEmitter;
+  @ViewChild('mapContainer') mapContainer: ElementRef;
+
+  public map: google.maps.Map;
+  public markers: google.maps.Marker[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    
+
+  }
+}
